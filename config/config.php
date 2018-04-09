cbe335be635edac4d33027f5ea068ae9cad4e854
NDTeam <zellkon@live.com>
2018-04-10 02:00:17 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', "sql12.freemysqlhosting.net");
-define('DB_USER', "sql12229514");
-define('DB_PASS', "naXJTHKduG");
-define('DB_NAME', "sql12229514");
+define('DB_HOST', "localhost");
+define('DB_USER', "root");
+define('DB_PASS', "123456");
+define('DB_NAME', "eta");
 ?>
